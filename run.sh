850da98d10d50d1dcf4e9b712ff9d8b6fff28fc6
vthirumaraiselvan <vthirumaraiselvan@shutterfly.com>
2018-04-06 11:50:20 -0700

    Add Server Running Commands

---
+++ run.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install mysql-server
+sudo apt-get -y install git
+git clone https://github.com/vidhya567/postSchoolsData.git
+cd postSchoolsData
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install
